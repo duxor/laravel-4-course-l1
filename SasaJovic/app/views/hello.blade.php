05433360976260e4f095669417b8b8a10c727a01
saxoman <jovicsasajovic@gmail.com>
2015-02-17 20:25:11 +0100

    Lekcija11
    HTMLBuilder

---
+++ SasaJovic/app/views/hello.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	{{ HTML:: script('js/script.js')}}
+	{{HTML:: style('css/style.css')}}
+
+
+</head>
+<body>
+
+{{HTML::link('http://google.rs','Google')}}
+{{HTML:: ul($list)}}
+
+
+
+
+</body>
+</html>
--- SasaJovic/resources/views/about.blade.php
+++
@@ -1,9 +0,0 @@
-@extends ('layouts.master')
-@section('navigation-bar')
-	@parent
-	<a href="contact">Contact</a>
-@stop
-
-@section ('content')
-	Welcome to about page.
-@stop
